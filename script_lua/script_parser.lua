171bc9bc77d216cd4aab5380b410a0671cb097e5
Sebastiaan de Schaetzen <sebastiaan.de.schaetzen@gmail.com>
2018-05-07 11:51:29 +0200

    Improve error message when the source file could not be read

--- script_lua/script_parser.lua
+++ script_lua/script_parser.lua
@@ -2,7 +2,11 @@ function script.load(filename)
 	script.directory = filename:match("^(.*/)")
 	local env = script.env
 	--local script = assert(loadfile(filename, "=" .. filename, env))
-	local script = assert(load(string.dump(love.filesystem.load(filename)), "=" .. filename, "bt", env))
+	local code, err = love.filesystem.load(filename)
+	if code == null then
+		error("Error loading file: " .. tostring(err))
+	end
+	local script = assert(load(string.dump(code), "=" .. filename, "bt", env))
 	local obj = {
 		env = env,
 		script = coroutine.wrap(script)
